b02bde7c6552c8f73bb5a14f6a21876cbd3131b9
Stanley Ndagi <ndagis@gmail.com>
2023-04-17 00:31:50 +0300

    Add MIT license

--- docs/.vuepress/components/MyHomeFooter.vue
+++ docs/.vuepress/components/MyHomeFooter.vue
@@ -2,7 +2,7 @@
   <div class="push"></div>
   <div class="footer center">
     <div>
-      Released under the <a href="" target="_blank">MIT License</a>
+      Released under the <a href="https://choosealicense.com/licenses/mit/" target="_blank">MIT License</a>
     </div>
     <div>
       By <a href="https://md.engineer" target="_blank">Stan_MD</a> | AD {{ year }}
